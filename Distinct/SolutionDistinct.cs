f22d19d13fe516d36413b2b3956839a691314314
Virgiliu Plesca <virgiliu.plesca@iis.utm.md>
2023-03-29 14:35:12 +0300

    Distinct done

---
+++ Distinct/SolutionDistinct.cs
@@ -0,0 +1,22 @@
+﻿namespace Distinct;
+
+// Counts the number of distinct elements in a array
+public class SolutionDistinct
+{
+    public int Solution(int[] A)
+    {
+        var temp = new HashSet<int>();
+        var nrDistinct = 0;
+        
+        for (int i = 0; i < A.Length; i++)
+        {
+            if (!temp.Contains(A[i]))
+            {
+                temp.Add(A[i]);
+                nrDistinct++;
+            }
+        }
+
+        return nrDistinct;
+    }
+}
