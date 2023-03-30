d820b45039e4eb45f7ff78270fd50fc9a037ab88
Virgiliu Plesca <virgiliu.plesca@iis.utm.md>
2023-03-30 16:46:25 +0300

    Passed cars changed class name and added maxPassedCars const for that extra magic number ;)

--- PassingCars/Solution.cs
+++ PassingCars/Solution.cs
@@ -1,16 +1,17 @@
 ﻿namespace PassingCars;
 
-public class SolutionClass
+public class PassingCarsSolutionClass
 {
-    public int Solution(int[] A)
+    public int Solution(int[] a)
     {
-        var n = A.Length;
+        var n = a.Length;
         var passedCars = 0;
         var counter = 0;
+        const int maxPassedCars = 1000000000;
         
         for (int i = 0; i < n; i++)
         {
-            if (A[i] == 0)
+            if (a[i] == 0)
             {
                 counter++;
             }
@@ -20,7 +21,7 @@ public class SolutionClass
             }
         }
 
-        if (passedCars > 1000000000 || passedCars < 0)
+        if (passedCars > maxPassedCars || passedCars < 0)
         {
             return -1;
         }
