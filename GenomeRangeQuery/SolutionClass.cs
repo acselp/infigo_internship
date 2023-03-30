3e781e21d0ea3eb6e2d5d5b1b6c9f07e00642460
Virgiliu Plesca <virgiliu.plesca@iis.utm.md>
2023-03-30 11:28:02 +0300

    genome done 62% TO DO: Better time complexity

---
+++ GenomeRangeQuery/SolutionClass.cs
@@ -0,0 +1,30 @@
+﻿namespace MissingIntegers;
+
+public class SolutionClass
+{
+    public int[] Solution(string genome, int[] p, int[] q)
+    {
+        string impactFactors = "ACGT";
+        var m = p.Length;
+        int[] response = new int[m];
+        var responseIndex = 0;
+
+        for (int i = 0; i < m; i++)
+        {
+            int minNucleotide = Int32.MaxValue;
+
+            for (int j = p[i]; j <= q[i]; j++)
+            {
+                if ((impactFactors.IndexOf(genome[j]) + 1) < minNucleotide)
+                {
+                    minNucleotide = (impactFactors.IndexOf(genome[j]) + 1);
+                }
+            }
+
+            response[responseIndex] = minNucleotide;
+            responseIndex++;
+        }
+
+        return response;
+    }
+}
