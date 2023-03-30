ced8631913df12118e5abeaea8a58cc14b4bc5f0
Virgiliu Plesca <virgiliu.plesca@iis.utm.md>
2023-03-30 17:01:32 +0300

    Changed the class name and renamed the method parameter name

--- MissingInteger/Program.cs
+++ MissingInteger/Program.cs
@@ -2,13 +2,14 @@
 
 public static class MissingIntegersClass
 {
-    public static SolutionClass Sol = new SolutionClass();
-    public static int[] Arr = { -1 };
+    public static MissingIntegerSolutionClass Sol = new MissingIntegerSolutionClass();
+    //This array will be checked for missing elements (in this example the missing element is 5)
+    public static int[] ArrayWithTheElements = { 1, 3, 6, 4, 1, 2 };
 
     public static void Main(String[] args)
     {
-        var a = Sol.Solution(Arr);
+        var array = Sol.MissingIntegerSolution(ArrayWithTheElements);
         
-        Console.Write(a);
+        Console.Write(array);
     }
 }
--- MissingInteger/Solution.cs
+++ MissingInteger/Solution.cs
@@ -1,18 +1,18 @@
 ﻿namespace MissingIntegers;
 
-public class SolutionClass
+public class MissingIntegerSolutionClass
 {
-    public int Solution(int[] a)
+    public int MissingIntegerSolution(int[] arrayWithTheElements)
     {
-        var n = a.Length;
+        var n = arrayWithTheElements.Length;
         int[] numsPresent = new int[n + 1];
 
         for (int i = 0; i < n; i++)
             numsPresent[i] = 0;
 
         for (int i = 0; i < n; i++)
-            if(a[i] > 0 && a[i] < n + 1) 
-                numsPresent[a[i] - 1] = 1;
+            if(arrayWithTheElements[i] > 0 && arrayWithTheElements[i] < n + 1) 
+                numsPresent[arrayWithTheElements[i] - 1] = 1;
         
         for (int i = 0; i <= n + 1; i++)
             if (numsPresent[i] == 0)
