69abc2931994b9aefea63081183095e799a9673b
Virgiliu Plesca <virgiliu.plesca@iis.utm.md>
2023-03-30 13:02:48 +0300

    MaxProductOfThre DONE 100%

---
+++ MaxProductOfThree/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MaxProductOfThree;
+
+public static class MaxProductOfThreeClass
+{
+    public static SolutionClass SolutionInstance = new();
+    public static int[] A = { -3, 1, 2, -2, 5, 6 };
+
+    public static void Main(String[] args)
+    {
+        var a = SolutionInstance.Solution(A);
+        
+        Console.Write(a);
+    }
+}
---
+++ MaxProductOfThree/SolutionClass.cs
@@ -0,0 +1,22 @@
+﻿namespace MaxProductOfThree;
+
+public class SolutionClass
+{
+    public int Solution(int[] a)
+    {
+        var n = a.Length;
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = i; j < n - i - 1; j++)
+            {
+                if (a[i] > a[i + 1])
+                {
+                    (a[i], a[i + 1]) = (a[i + 1], a[i]);
+                }
+            }
+        }
+
+        return a[n - 1] * a[n - 2] * a[n - 3];
+    }
+}
